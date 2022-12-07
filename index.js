3521e79d25ed5ab3736f789f5708cdde07113d9e
CameronStClair95 <cameronaitcheson@gmail.com>
2022-12-07 16:06:21 +0000

    Working on Lab not finished yet

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+// Iteration 1: Constructor()
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  
+// Iteration 2: add(items)
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  // When you add an the items, use the push method to add the item to the array. 
+  // After this has been done, you can and should use the sort method to sort the array in the correct order, in this case, ascending order.
+  // Lastly, it is important to update the length of the array so that it matches the new length of the array. 
 
-  add(item) {}
+// Iteration 3: get(pos)
+get(pos) {}
+ 
 
-  get(pos) {}
-
+// Iteration 4: max()
   max() {}
-
+   
+// Iteration 5: min()
   min() {}
-
+  
+// Iteration 6: sum()
   sum() {}
 
   avg() {}
